fbe04fa8d3a1fa468f96b49f998e760e8cae78a2
DEEPAK-DEV2024 <158258171+DEEPAK-DEV2024@users.noreply.github.com>
2024-08-23 06:11:49 +0100

    Update emailjs to web3form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,29 +1,32 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from 'react';
+import { toast } from 'react-toastify';
+import env from 'react-dotenv';
 
-export const ContactUs = () => {
-  const form = useRef();
+const ContactUs = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    formData.append("access_key",import.meta.env.VITE_ACCESS_KEY);
 
-    emailjs
-      .sendForm('', 'service_raa7z0s', form.current, {
-        publicKey: 'DSA5AwsvpZZ9m7Yh_',
-      })
-      .then(
-        () => {
-          form.current.reset();
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success("Form Successfully Submitted")
+      event.target.reset();
+    } else {
+      toast.error("Form Not Submited")
+    }
   };
 
   return (
     <section className='shadow-[5px_5px_50px_0px_rgba(0,0,0,.2),-5px_-5px_20px_0px_rgba(255,255,255,.1)] rounded-md py-4 px-6 mb-12 w-4/5 mx-auto mt-4'>
-    <form ref={form} onSubmit={sendEmail} className="space-y-6">
+    <form  onSubmit={onSubmit}>
         <div>
             <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="user_name">Name</label>
             <input type="text" name="user_name" id="user_name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline bg-transparent"  required/>
@@ -48,3 +51,5 @@ export const ContactUs = () => {
 
   );
 };
+
+export default ContactUs
