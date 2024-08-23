6a09b50443447c08f9aa18bc6566029604f09c26
DEEPAK-DEV2024 <158258171+DEEPAK-DEV2024@users.noreply.github.com>
2024-08-23 07:19:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import AboutMe from './Components/AboutMe';
 import Skill from './Components/Skill';
 import Services from './Components/Services';
 import ContactUs from './Components/Contact';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+    <>
     <div className='md:flex justify-between md:px-20 md:pr-0'>
       <SideBar />
       <div className='md:w-9/12 w-full pt-10 h-[100vh] md:overflow-y-auto md:pr-14'>
@@ -16,6 +19,8 @@ function App() {
         <ContactUs />
       </div>
     </div>
+    <ToastContainer />
+      </>
   );
 }
 
