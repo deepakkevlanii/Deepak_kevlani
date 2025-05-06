83c434f53584b0b75cabe5fbe1b23a92a57fff59
Deepak Kevlani <158258171+deepakkevlanii@users.noreply.github.com>
2025-05-06 15:41:01 +0530

    ServiceCard update

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ServiceCard = ({ imgsrc, title, description }) => {
   return (
-    <div className="service-card relative bg-transparent p-8 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 mb-5" style="width:100%;">
+    <div className="service-card relative bg-transparent p-8 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 mb-5 w-full">
       <div className="icon-wrapper bg-white text-pink-500 p-4 rounded-full shadow-lg mx-auto mb-4 text-4xl h-20 w-20">
         <img src={`/images/${imgsrc}.png`} alt="" />
       </div>
